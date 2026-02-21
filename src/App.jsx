b957f910ac5d6087ba4e9c17943dd3b631398f51
Tamar Grossman <tamar.yust@gmail.com>
2026-02-21 18:31:06 +0200

    fixing import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Button, Container, Typography } from '@mui/material'
 import { Dashboard } from './components/Dashboard';
 import { mockTestCases, mockUsers, mockProjects } from './data/mockData';
 import { theme} from './theme'
+import { ThemeProvider } from '@mui/material/styles'
 
 function App() {
   return (
